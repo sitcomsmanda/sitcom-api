df683752ffa21146eca3f8b7800bae13f973ed7c
DikDns <dikdnssocial@gmail.com>
2022-07-24 14:06:42 +0700

    add: favicon

--- pages/api-docs.js
+++ pages/api-docs.js
@@ -1,6 +1,12 @@
 import Head from "next/head";
 
-const App = () => {
+import { createSwaggerSpec } from "next-swagger-doc";
+import dynamic from "next/dynamic";
+import "swagger-ui-react/swagger-ui.css";
+
+const SwaggerUI = dynamic(import("swagger-ui-react"), { ssr: false });
+
+function ApiDoc({ spec }) {
   return (
     <div>
       <Head>
@@ -23,8 +29,27 @@ const App = () => {
         ></link>
         <link rel="manifest" href="./favicon/site.webmanifest"></link>
       </Head>
+      <SwaggerUI spec={spec} />
     </div>
   );
-};
+}
 
-export default App;
+export async function getStaticProps() {
+  const spec = createSwaggerSpec({
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "SITCOM API",
+        version: "0.0.1",
+      },
+    },
+  });
+
+  return {
+    props: {
+      spec,
+    },
+  };
+}
+
+export default ApiDoc;
