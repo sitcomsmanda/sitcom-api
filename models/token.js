c55625e249cd59611a5e344f788cbf80670462ec
ADMINISTRATOR SITCOM <102400694+admin-sitcom@users.noreply.github.com>
2022-07-28 18:46:54 +0700

    add: temp token models

---
+++ models/token.js
@@ -0,0 +1,30 @@
+// import mongoose from "mongoose";
+
+// const tokenSchema = new mongoose.Schema({
+//   token: {
+//     type: String,
+//     required: true,
+//     immutable: true,
+//     default: () => new Date.now()
+//   },
+//   status: {
+//     type: Boolean,
+//     required: true,
+//   },
+//   createdAt: {
+//     type: Date,
+//     immutable: true,
+//     default: () => new Date.now(),
+//   },
+//   updatedAt: {
+//     type: Date,
+//     default: () => new Date.now(),
+//   },
+// });
+
+// tokenSchema.pre("save", function (next) {
+//   this.updatedAt = new Date.now();
+//   next();
+// });
+
+// export default mongoose.models.Token || mongoose.model("Token", tokenSchema);
